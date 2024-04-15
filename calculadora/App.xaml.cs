ace86cb97102602dc5fac823393d683cdc7e694d
Usuario <Usuario@CPD03-M14>
2024-04-15 15:45:52 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace calculadora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
